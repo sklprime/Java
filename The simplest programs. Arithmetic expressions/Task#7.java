c97e59816a532bae3ee1e3a11915759a09e784b5
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 15:02:10 +0200

    Create Task#7.java

---
+++ The simplest programs. Arithmetic expressions/Task#7.java
@@ -0,0 +1,20 @@
+/* Task # 7. Make an output program on the screen in one line
+ *  of any three numbers with two spaces between them.
+ * Задача № 7. Составить программу вывода на экран в одну строку трех любых чисел
+ *  с двумя пробелами между ними.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne = 157;
+		double numTwo = 56.458;
+		int numThree = 123456;
+
+		System.out.println(numOne + "  " + numTwo + "  " + numThree);
+
+	}
+
+}
