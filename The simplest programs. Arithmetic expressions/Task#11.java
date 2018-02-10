3275603983938aa367e4cc915756671f65503b15
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 15:26:00 +0200

    Create Task#11.java

---
+++ The simplest programs. Arithmetic expressions/Task#11.java
@@ -0,0 +1,27 @@
+/* Task # 11. To make the program of output on the screen "a column" of any four numbers.
+ * Задача № 11. Составить программу вывода на экран "столбиком" четырех любых чисел.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne;
+		numOne = 100;
+		double numTwo;
+		numTwo = 10.56;
+		int numThree;
+		numThree = 21569;
+		double numFour;
+		numFour = 456.238;
+
+		// System.out.println(numOne);
+		// System.out.println(numTwo);
+		// System.out.println(numThree);
+		// System.out.println(numFour);
+		System.out.println(numOne + "\n" + numTwo + "\n" + numThree + "\n" + numFour);
+
+	}
+
+}
