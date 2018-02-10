1ed76abbe6bcc66fe4139d5a3d8c096d2610d6ac
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 14:43:57 +0200

    Create Task#6.java

---
+++ The simplest programs. Arithmetic expressions/Task#6.java
@@ -0,0 +1,18 @@
+/* Task # 6. Output on a single line the numbers 7, 15 and 100 with two spaces between them.
+ * Задача № 6. Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne = 7;
+		int numTwo = 15;
+		int numThree = 100;
+
+		System.out.println(numOne + "  " + numTwo + "  " + numThree);
+
+	}
+
+}
