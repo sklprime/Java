42fd21dcb8de6768b3915fa1f2a134f2e8e51f5a
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 15:21:29 +0200

    Create Task#10.java

---
+++ The simplest programs. Arithmetic expressions/Task#10.java
@@ -0,0 +1,24 @@
+/* Task # 10. Display the numbers 5, 10 and 21 one under the other.
+ * Задача № 10. Вывести на экран числа 5, 10 и 21 одно под другим.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne;
+		numOne = 5;
+		int numTwo;
+		numTwo = 10;
+		int numThree;
+		numThree = 21;
+
+		// System.out.println(numOne);
+		// System.out.println(numTwo);
+		// System.out.println(numThree);
+		System.out.println(numOne + "\n" + numTwo + "\n" + numThree);
+
+	}
+
+}
