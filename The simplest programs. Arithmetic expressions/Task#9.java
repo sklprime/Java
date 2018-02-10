3c6259d21d1a9042aec1701b03227d3795a86189
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 15:12:28 +0200

    Create Task#9.java

---
+++ The simplest programs. Arithmetic expressions/Task#9.java
@@ -0,0 +1,20 @@
+/* Task # 9. Display numbers 50 and 10 one under the other.
+ * Задача № 9. Вывести на экран числа 50 и 10 одно под другим.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne;
+		numOne = 50;
+		int numTwo;
+		numTwo = 10;
+
+		System.out.println(numOne);
+		System.out.println(numTwo);
+
+	}
+
+}
