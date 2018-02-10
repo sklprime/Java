a3b8cfe883fa18603fefadd1f0a602209fc991d7
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 14:40:54 +0200

    Create Task#5.java

---
+++ The simplest programs. Arithmetic expressions/Task#5.java
@@ -0,0 +1,18 @@
+/* Task # 5. Output on a single line the numbers 1, 13 and 49 with one space between them.
+ * Задача № 5. Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne = 1;
+		int numTwo = 13;
+		int numThree = 49;
+
+		System.out.println(numOne + " " + numTwo + " " + numThree);
+
+	}
+
+}
