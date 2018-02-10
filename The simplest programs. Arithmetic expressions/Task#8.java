34415a061ddb89b42c713212ab424a739ff8ebff
sklprime <35468130+sklprime@users.noreply.github.com>
2018-02-10 15:07:04 +0200

    Create Task#8.java

---
+++ The simplest programs. Arithmetic expressions/Task#8.java
@@ -0,0 +1,21 @@
+/* Task # 8. Create an output program on the screen in one line
+ *  of four any numbers with one space between them.
+ * Задача № 8. Составить программу вывода на экран в одну строку
+ *  четырех любых чисел с одним пробелом между ними.*/
+
+package com.gmail.sklprime;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int numOne = 157;
+		double numTwo = 56.458;
+		int numThree = 123456;
+		double numFour = 0.4569;
+
+		System.out.println(numOne + " " + numTwo + " " + numThree + " " + numFour);
+
+	}
+
+}
